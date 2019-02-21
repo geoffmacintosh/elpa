bcb924815f16270218a4a7359bbe37f68788477a
Geoff MacIntosh <geoff@mac.into.sh>
2019-02-20 17:37:01 -0700

    Initial commit

---
+++ prescient-20190217.308/prescient-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "prescient" "20190217.308" "Better sorting and filtering." '((emacs "25.1")) :commit "b949666b14ac0eb3ac8d1dc2e3baf7062d2a436c" :keywords '("extensions") :authors '(("Radon Rosborough" . "radon.neon@gmail.com")) :maintainer '("Radon Rosborough" . "radon.neon@gmail.com") :url "https://github.com/raxod502/prescient.el")
