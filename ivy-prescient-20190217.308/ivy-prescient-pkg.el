7e38f9a99c7b61cffabe24b74dc746d527ba9049
Geoff MacIntosh <geoff@mac.into.sh>
2019-02-24 12:17:02 -0700

    Update packages

--- ivy-prescient-20190217.308/ivy-prescient-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ivy-prescient" "20190217.308" "prescient.el + Ivy" '((emacs "25.1") (prescient "2.2.2") (ivy "0.11.0")) :commit "b949666b14ac0eb3ac8d1dc2e3baf7062d2a436c" :keywords '("extensions") :authors '(("Radon Rosborough" . "radon.neon@gmail.com")) :maintainer '("Radon Rosborough" . "radon.neon@gmail.com") :url "https://github.com/raxod502/prescient.el")
