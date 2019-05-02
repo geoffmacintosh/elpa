2974c7c4382a18d96c0ff52309de3669c63b1733
Geoff MacIntosh <geoff@mac.into.sh>
2019-05-02 16:36:52 -0600

    Update packages

--- company-prescient-20190213.635/company-prescient-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "company-prescient" "20190213.635" "prescient.el + Company" '((emacs "25.1") (prescient "2.2.2") (company "0.9.6")) :commit "b949666b14ac0eb3ac8d1dc2e3baf7062d2a436c" :keywords '("extensions") :authors '(("Radon Rosborough" . "radon.neon@gmail.com")) :maintainer '("Radon Rosborough" . "radon.neon@gmail.com") :url "https://github.com/raxod502/prescient.el")
